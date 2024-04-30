3364e5ccb0e72702c26c91e699c0a5c60a2b0f1d
KruASe76 <kruanse@ya.ru>
2024-04-30 16:59:02 +0300

    Player.isInGame fix

--- src/main/kotlin/me/kruase/minenopoly/util/PlayerExt.kt
+++ src/main/kotlin/me/kruase/minenopoly/util/PlayerExt.kt
@@ -5,7 +5,14 @@ import me.kruase.minenopoly.Minenopoly.Companion.userConfig
 import org.bukkit.entity.Player
 
 
-fun Player.isInGame(): Boolean {
-    return inventory.contents.any { it?.itemMeta?.persistentDataContainer?.hasAnyMark() == true } ||
-            (gameData?.bank?.location?.distance(location) ?: Double.POSITIVE_INFINITY) < userConfig.gameDistance
-}
+val Player.isInGame: Boolean
+    get() =
+        gameData != null &&
+                (
+                        inventory.contents
+                            .any { it?.itemMeta?.persistentDataContainer?.hasAnyMark() == true } ||
+                                gameData?.bank?.location?.let { bankLocation ->
+                                    bankLocation.world == location.world &&
+                                            bankLocation.distanceSquared(location) < userConfig.gameDistanceSquared
+                                } ?: false
+                        )
